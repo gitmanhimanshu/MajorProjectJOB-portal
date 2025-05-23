8050808f032c9838b195ecc430c5531e021da58b
himanshu <himanshu.prpwebs@gmail.com>
2025-05-23 21:08:17 +0530

    cnages in logout button

--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -3,16 +3,35 @@ import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button' 
 import { useNavigate } from 'react-router-dom' 
-import { useDispatch } from 'react-redux' 
+import { useDispatch, useSelector } from 'react-redux' 
 import AdminJobsTable from './AdminJobsTable'
 import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
 import { setSearchJobByText } from '@/redux/jobSlice'
+import { LogOut } from 'lucide-react'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
 
 const AdminJobs = () => {
   useGetAllAdminJobs();
   const [input, setInput] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+      if (res.data.success) {
+        dispatch(setUser(null));
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  }
 
   useEffect(() => {
     dispatch(setSearchJobByText(input));
@@ -27,7 +46,12 @@ const AdminJobs = () => {
             placeholder="Filter by name, role"
             onChange={(e) => setInput(e.target.value)}
           />
-          <Button onClick={() => navigate("/admin/jobs/create")}>New Jobs</Button>
+          <div className='flex gap-2'>
+            <Button onClick={() => navigate("/admin/jobs/create")}>New Jobs</Button>
+            <Button onClick={logoutHandler} variant="destructive" className="flex items-center gap-2">
+              <LogOut className="w-4 h-4" /> Logout
+            </Button>
+          </div>
         </div>
         <AdminJobsTable />
       </div>
